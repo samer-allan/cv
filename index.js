f14c00c41e9ed322cc60423f7ef5999287aa4bb6
samer-allan <90357612+samer-allan@users.noreply.github.com>
2021-11-24 13:26:09 +0200

    new feature index.js

--- index.js
+++ index.js
@@ -1,288 +1,169 @@
-body {
-    text-align: center;
-    font-family:"Comic Sans MS" ,sans-serif;
+const button = document.querySelectorAll(".piano")
+for (let i=0; i < button.length; i++){
+     button[i].addEventListener("click",function() {
+
+          var buttonInnerHTML = this.innerHTML
+          makeSound(buttonInnerHTML)
+          buttonAnimation(buttonInnerHTML)
+
+
+     });
 }
 
-h1 {
-    flex: 1;
-    font-family: 'Sacramento', cursive;
-    font-size: 6rem;
-    color: rgb(72, 121, 122);
-    margin-top: 100px;
+addEventListener("keypress", function(event){
+     makeSound(event.key)
+
+     buttonAnimation(event.key)
+})
+
+function makeSound(buttonInnerHTML){
+
+     
+
+     switch (buttonInnerHTML) {
+          case 'C':
+          var c = new Audio("c.m4a")
+          c.play()
+          
+               break;
+
+          case 'Db':
+          var db = new Audio("db.m4a")
+          db.play()
+               break;
+
+          case 'D':
+          var d = new Audio("d.m4a")
+          d.play()
+               break;
+
+          case 'Eb':
+          var eb = new Audio("eb.m4a")
+          eb.play()
+               break;
+
+          case 'E':
+          var e = new Audio("e.m4a")
+          e.play()
+               break;
+
+          case 'F':
+          var f = new Audio("f.m4a")
+          f.play()
+               break;
+          
+          case 'Gb':
+          var gb = new Audio("gb.m4a")
+          gb.play()
+               break;
+
+          case 'G':
+          var g = new Audio("g.m4a")
+          g.play()
+               break;
+
+          case 'Ab':
+          var ab = new Audio("ab.m4a")
+          ab.play()
+               break;
+
+          case 'A':
+          var a = new Audio("a.m4a")
+          a.play()
+               break;
+
+          case 'Bb':
+          var bb = new Audio("bb.m4a")
+          bb.play()
+               break;
+                         
+          case 'B':
+          var b = new Audio("b.m4a")
+          b.play()
+               break; 
+          
+          case 'c':
+          var cc = new Audio("cc.m4a")
+          cc.play()
+               break; 
+     
+     default:
+          case 's':
+          var c = new Audio("c.m4a")
+          c.play()
+          
+               break;
+
+          case 'e':
+          var db = new Audio("db.m4a")
+          db.play()
+               break;
+
+          case 'd':
+          var d = new Audio("d.m4a")
+          d.play()
+               break;
+
+          case 'r':
+          var eb = new Audio("eb.m4a")
+          eb.play()
+               break;
+
+          case 'f':
+          var e = new Audio("e.m4a")
+          e.play()
+               break;
+
+          case 'g':
+          var f = new Audio("f.m4a")
+          f.play()
+               break;
+          
+          case 'y':
+          var gb = new Audio("gb.m4a")
+          gb.play()
+               break;
+
+          case 'h':
+          var g = new Audio("g.m4a")
+          g.play()
+               break;
+
+          case 'u':
+          var ab = new Audio("ab.m4a")
+          ab.play()
+               break;
+
+          case 'j':
+          var a = new Audio("a.m4a")
+          a.play()
+               break;
+
+          case 'i':
+          var bb = new Audio("bb.m4a")
+          bb.play()
+               break;
+                         
+          case 'k':
+          var b = new Audio("b.m4a")
+          b.play()
+               break; 
+          
+          case 'l':
+          var cc = new Audio("cc.m4a")
+          cc.play()
+               break;
+
+     }
+
 }
 
-p { 
-    font-size: 20px;
-}
+function buttonAnimation(keypressed) {
 
-
-.intro {
-    flex: 1;
-    margin-top: 5%;
-    margin-left: 20%;
-    width: 60% ;
+     
+     var activeButton = document.querySelector("." + keypressed)
+     activeButton.classList.add("pressed")
+     setTimeout(function (){
+          activeButton.classList.remove("pressed")}, 100)
 
 }
-h3 {
-    font-size: 1.4rem;
-}
-
-h2 {
-    flex: 1;
-    font-family: Helvetica, sans-serif;
-    color: rgb(75, 182, 224);
-    font-size: 2.5rem;
-
-}
-hr{
-    border: dotted steelblue 6px;
-    border-bottom: none;
-    width: 4%;
-    margin: 80px auto;
-}
-
-.container {
-    display: flex;
-    justify-content: center;
-    background: #91bdda;
-    flex-direction: column;
-}
-
-.container div {
-    padding: 20px;
-
-}
-.middle-container {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 70px 0;
-}
-
-.contact-message{ 
-    width: 50%;
-    margin: 10px auto 30px;
-}
-.bottom-container {
-    padding: 50px 0 20px;
-    margin: 0;
-    background:rgb(110, 172, 223);
-}
-
-.skill-row {
-    flex: 1;
-    width: 80%;
-    margin: 10px auto 100px auto;
-    text-align: left;
-    line-height: 2;
-
-}
-.skills {
-    flex: 1;
-    margin: 10px;
-}
-.mine {
-    flex: 1;
-    margin-left: 20px;
-    margin-bottom: 25px;
-}
-
-.art-img {
-    flex: 1;
-    float: left;
-    width: 30%;
-    border-radius: 100px; 
-    margin-right: 30px;
-}
-
-.computer-img {
-    flex: 1;
-    position: relative;
-    float: right;
-    width: 25%;
-    border-radius: 4cm;
-
-}
-.programming {
-    flex: 1;
-
-}
-
-#my-skills{
-    flex: 3;
-    position: relative;
-
-}
-.pro {
-    color: #0f455e;
-    font-size: 1.5rem;
-
-}
-
-.top-cloud {
-    position: absolute;
-    flex: 1;
-    top: 200px ;
-    opacity: 0.5;
-
-}
-.bottom-cloud {
-    position: absolute;
-    opacity: 0.7;
-    top: 400px;
-
-}
-
-#mountain {
-    flex: 1;
-    width: 45%;
-    text-align: center;
-
-}
-
-.btn {
-    background: #11cdd4;
-    border-radius: 8px;
-    color: #ffffff;
-    font-size: 20px;
-    padding: 10px 20px 10px 20px;
-    text-decoration: none;
-}
-.btn:hover {
-    background: #30e3cb;
-    text-decoration: none;
-}
-
-a {
-    text-decoration: none;
-    font-family: sans-serif;
-    padding: 5px;
-    margin:5px 5px ;
-    color: rgb(52, 81, 90);
-    cursor: pointer;
-}
-a:hover{
-    color: #eaf6f6;
-}
-
-.material-icons {
-    position: relative;
-    bottom: 20px;
-    color: #eaf6f6;
-}
-#iconss {
-    color: honeydew;
-    position: relative;
-    margin-bottom: 5%;
-}
-
-
-
-/* ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ */
-  
-  .container-piano {
-    display: flex;
-    text-align: center;
-    flex: 1;
-    justify-content: center;
-    
-  }
-  
-  
-  .s {
-    background-color:whitesmoke;
-  }
-  
-  .e {
-    background-color:black;
-  
-  }
-  
-  .d {
-    background-color:whitesmoke;
-  
-  }
-  
-  .r {
-    background:black;
-  
-  }
-  
-  .f {
-    background:whitesmoke;
-  
-  }
-  
-  .g {
-    background:whitesmoke;
-  
-  }
-  
-  .y {
-    background:black;
-  }
-  
-  .h {
-    background:whitesmoke;
-  
-  }
-  .u {
-    background:black;
-  
-  }
-  .j {
-    background:whitesmoke;
-  
-  }
-  .i {
-    background:black;
-  
-  }
-  .k {
-    background:whitesmoke;
-    
-  }
-  
-  .l {
-  background:whitesmoke;
-  }
-  
-  .pressed {
-    box-shadow: 0 3px 4px 0 #aebfc5 ;
-    opacity: 0.1;
-  }
-  
-  
-  
-  
-  .white {
-    outline: none;
-    border: 2px solid #404B69;
-    font-size: 1rem;
-    font-family: sans-serif;
-    font-weight: 100;
-    color: black;
-    border-radius: 5px;
-    width: 40px;
-    height: 180px;
-    line-height: 9cm;
-    margin: 0;
-  }
-  
-  .black {
-    position: absolute;
-    outline: none;
-    border: 2px solid #404B69;
-    font-weight: 50px;
-    color:whitesmoke;
-    border-radius: 10px;
-    width: 25px;
-    height: 110px;
-    margin: 0;
-  }
-  
-  #blk {
-    position: relative;
-    right: 12px;
-  }
-  
